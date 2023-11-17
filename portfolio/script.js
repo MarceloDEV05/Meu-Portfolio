52ec0a7299e60e0590a20f17676e73abd230e1ec
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2023-11-17 16:11:54 -0300

    portfolio pronto

---
+++ portfolio/script.js
@@ -0,0 +1,30 @@
+
+
+let btn = document.getElementById('btn-dark')
+let btnLight = document.getElementById('btn-light')
+let body = document.querySelector('body')
+let img = document.getElementById('imagem')
+let header = document.getElementById('head')
+
+
+btn.addEventListener('click', trocaCor)
+btnLight.addEventListener('click', retornaCor)
+
+function trocaCor() {
+    body.style.background = 'black'
+    body.style.color = 'white'
+    btn.style.color = 'white'
+    btnLight.style.color = 'white'
+    header.src = '/portfolio/img/transferir.png'
+
+
+}
+
+function retornaCor() {
+    body.style.background = 'white'
+    body.style.color = 'black'
+    btnLight.style.color = 'black'
+    btn.style.color = 'black'
+    header.src ='/portfolio/img/6528597.png'
+    
+}
