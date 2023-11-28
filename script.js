b9e1cc635243609d10ef3ba8924f6b47a50cc538
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2023-11-28 15:02:44 -0300

    melhorando portfolio

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-
-
-let btn = document.getElementById('btn-dark')
-let btnLight = document.getElementById('btn-light')
-let body = document.querySelector('body')
-let img = document.getElementById('imagem')
-let header = document.getElementById('head')
-
-
-
-btn.addEventListener('click', trocaCor)
-btnLight.addEventListener('click', retornaCor)
-
-function trocaCor() {
-    body.style.background = 'black'
-    body.style.color = 'white'
-    btn.style.color = 'white'
-    btnLight.style.color = 'white'
-    header.src = '/img/transferir.png'
-
-
-}
-
-function retornaCor() {
-    body.style.background = 'white'
-    body.style.color = 'black'
-    btnLight.style.color = 'black'
-    btn.style.color = 'black'
-    header.src ='/img/6528597.png'
-    
+function cliqueMenu(){
+    var body = document.querySelector('body')
+    var item = document.querySelector('ul')
+    if( item.style.display == 'block'){
+        item.style.display = 'none'
+        
+        
+    }else{
+        item.style.display = 'block'
+       
+       
+    }
+   
 }
