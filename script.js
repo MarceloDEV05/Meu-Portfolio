7b7369ff7bb2d8159b9e292c4df6db2262dd1243
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2024-06-29 20:08:38 -0300

    editando e atualizando portfolio

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function cliqueMenu(){
-    var body = document.querySelector('body')
-    var item = document.querySelector('ul')
-    if( item.style.display == 'block'){
-        item.style.display = 'none'
-        
-        
-    }else{
-        item.style.display = 'block'
-       
-       
-    }
-   
-}
+
